2d20c33e8f793574b452b2c69a1cdd013d57d7f1
unknown <thegalaxywolf12345@gmail.com>
2020-11-02 22:16:00 +0200

    Progress

--- index.js
+++ index.js
@@ -5,72 +5,72 @@ canvas.width = innerWidth
 canvas.height = innerHeight
 
 class Player {
-   constructor (x, y, radius, color) {
-       console.log(x)
-       this.x = x
-       this.y = y
-       this.radius = radius
-       this.color = color
-   }
+    constructor(x, y, radius, color) {
+        console.log(x)
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
 
-   draw() {
+    draw() {
         c.beginPath()
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         c.fillStyle = this.color
         c.fill()
-  }
+    }
 }
-  
+
 class Projectile {
     constructor(x, y, radius, color,
         velocity) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-    this.velocity = velocity
-}
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
 
-draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-   }
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
 
-   update() {
-       this.draw()
-       this.x = this.x + this.velocity.
-         x
-         this.y = this.y + this.velocity.
-         y
-   }
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.
+            x
+        this.y = this.y + this.velocity.
+            y
+    }
 }
 
 class Enemy {
     constructor(x, y, radius, color,
         velocity) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-    this.velocity = velocity
-}
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
 
-draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-   }
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
 
-   update() {
-       this.draw()
-       this.x = this.x + this.velocity.
-         x
-         this.y = this.y + this.velocity.
-         y
-   }
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.
+            x
+        this.y = this.y + this.velocity.
+            y
+    }
 }
 
 const x = canvas.width / 2
@@ -79,52 +79,105 @@ const y = canvas.height / 2
 const player = new Player(x, y, 30, 'red')
 const projectiles = []
 const enemies = []
-function spawnEnemies () {
-   setInterval(() => {
-      const x = 100 
-      const y = 100 
-      const radius = 30
-      const color = 'green'
-      const velocity = {
-          x: 1,
-          y: 1
-      }
-    enemies.push(new Enemy(x, y, radius, color, velocity))
-    
-    console.log(enemies);
-   }, 1000)
-}
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = Math.random() * (50 - 10) + 10
 
-function animate () {
-    requestAnimationFrame(animate)
+        let x
+        let y
+
+
+        if (Math.random() < 0.5) {
+
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            y = Math.random() * canvas.height
+        } else {
+            x = Math.random() * canvas.width
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+        }
+
+
+
+        const color = 'green'
+
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
+
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+
+        }
+
+        enemies.push(new Enemy(x, y, radius, color,
+            velocity))
+    }, 1000)
+}
+let animationId
+function animate() {
+    animationId = requestAnimationFrame(animate)
+
+    //c.fillStyle = ''
+    //c.fillRect(0, 0, canvas.width, canvas.height)
+
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.draw()
-    projectiles.forEach(projectile =>
-      {
-      projectile.update()
-   });
+    projectiles.forEach((projectile, index) => {
+        projectile.update()
 
-   enemies.forEach(enemy => {
-      enemy.update()
-   });
+        //remove from edges of the screen
+      if  (projectile.x + projectile.radius < 0 ||
+           projectile.x - projectile.radius > canvas.width ||
+
+           projectile.y + projectile.radius < 0 ||
+           projectile.y - projectile.radius > canvas.height)
+        {
+            setTimeout(() => {
+                projectiles.splice(index, 1)
+            }, 0)    
+        }
+    })
+
+    enemies.forEach((enemy, index) => {
+        enemy.update()
+
+        const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+
+        // end game
+        if (dist - enemy.radius - player.radius < 1) {
+            cancelAnimationFrame(animationId)
+        }
+
+        projectiles.forEach((projectile, projectileIndex) => {
+
+            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+
+            // Objects Touch
+            if (dist - enemy.radius - projectile.radius < 1) {
+                setTimeout(() => {
+                    enemies.splice(index, 1)
+                    projectiles.splice(projectileIndex, 1)
+                }, 0)
+
+            }
+        });
+    })
 }
 
-
-addEventListener('click', (event) => 
-   {
-  const angle = Math.atan2(event.clientY - canvas.height / 2,
-    event.clientX - canvas.width / 2
+addEventListener('click', (event) => {
+    console.log(projectiles)
+    const angle = Math.atan2(event.clientY - canvas.height / 2,
+        event.clientX - canvas.width / 2
     )
     const velocity = {
         x: Math.cos(angle),
         y: Math.sin(angle)
-    
+
     }
     projectiles.push(new Projectile(
         canvas.width / 2, canvas.height / 2, 5, 'blue',
         velocity)
     )
-   })
+})
 
 animate()
 spawnEnemies() 
